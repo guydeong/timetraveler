c6da8fb122b6bce0ad5e24e201ea6a0f52f9037c
guydeong <Mjluzuriaga109@gmail.com>
2025-09-20 23:13:13 -0700

    WASM

---
+++ frontend/src/rust/target/wasm32-unknown-unknown/release/add_two_numbers.d
@@ -0,0 +1,1 @@
+/home/guydeong/CS144/final_project/team34/frontend/src/rust/target/wasm32-unknown-unknown/release/add_two_numbers.wasm: /home/guydeong/CS144/final_project/team34/frontend/src/rust/lib.rs
---
+++ frontend/src/rust/target/wasm32-unknown-unknown/release/deps/add_two_numbers.d
@@ -0,0 +1,5 @@
+/home/guydeong/CS144/final_project/team34/frontend/src/rust/target/wasm32-unknown-unknown/release/deps/add_two_numbers.wasm: lib.rs
+
+/home/guydeong/CS144/final_project/team34/frontend/src/rust/target/wasm32-unknown-unknown/release/deps/add_two_numbers.d: lib.rs
+
+lib.rs:
